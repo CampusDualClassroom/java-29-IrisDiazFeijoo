26bb5b248d5983726cc20c3fd6fc5a8e84879c2f
Iris Diaz Feijoo <irisdf_4@hotmail.com>
2024-10-30 17:06:16 +0100

    Terminado ejercicio 29

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,13 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            throw new RuntimeException("Error intencionado.");
+        } catch (RuntimeException e) {
+            System.out.println("Se ha capturado una excepción: " + e.getMessage());
+        } finally {
+            System.out.println("El programa se ha completado sin terminar abruptamente.");
+        }
     }
 }
+
